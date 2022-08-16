2dcf3175a888fd4c662228183abc8aca77ddcea3
Julio Franca <juliohcf@hotmail.com>
2022-08-16 16:57:06 -0300

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get "tasks", to: "tasks#index", as: :index
+
+  get "tasks/new", to: "tasks#new", as: :new
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
+
+  patch "tasks/:id", to: "tasks#update"
+
+  #PRA MOSTRAR O FORMULARIO É NEW, PRA ENVIAR O FORMULARIO É CREATE
+
+  post "tasks", to: "tasks#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
